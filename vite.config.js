15bfe5eeddf6eb2230e371c4b1742c5111cecf32
jfitt149 <jfitt149@gmail.com>
2024-07-14 13:49:44 -0300

    modified vitconfig to load env variables in scss

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          $api-url: "${process.env.VITE_API_URL}";
+        `,
+      },
+    },
+  },
+});
